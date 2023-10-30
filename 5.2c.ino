e070c743c049dc8d9953ca52a07f6ed11e72027e
udit1726 <133204047+udit1726@users.noreply.github.com>
2023-10-30 11:26:39 +0530

    Update 5.2c.ino

--- 5.2c.ino
+++ 5.2c.ino
@@ -1,73 +1,15 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QRadioButton, QPushButton, QVBoxLayout, QWidget
+import tkinter as tk
 import RPi.GPIO as GPIO
-# GPIO pins for LEDs
-RED_LED_PIN = 17
-GREEN_LED_PIN = 18
-BLUE_LED_PIN = 27
-
-# Initialize GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(RED_LED_PIN, GPIO.OUT)
-GPIO.setup(GREEN_LED_PIN, GPIO.OUT)
-GPIO.setup(BLUE_LED_PIN, GPIO.OUT)
-
-class LEDController(QMainWindow):
-    def _init_(self):
-        super()._init_()
-
-        # Create central widget
-        central_widget = QWidget(self)
-
-        # Create layout
-        layout = QVBoxLayout()
-
-        # Create radio buttons
-        self.red_button = QRadioButton("Red LED", central_widget)
-        self.green_button = QRadioButton("Green LED", central_widget)
-        self.blue_button = QRadioButton("Blue LED", central_widget)
-
-        # Create exit button
-        self.exit_button = QPushButton("Exit", central_widget)
-
-        # Connect button signals to slots
-        self.red_button.clicked.connect(self.set_red_led)
-        self.green_button.clicked.connect(self.set_green_led)
-        self.blue_button.clicked.connect(self.set_blue_led)
-        self.exit_button.clicked.connect(self.close)
-
-        # Add widgets to layout
-        layout.addWidget(self.red_button)
-        layout.addWidget(self.green_button)
-        layout.addWidget(self.blue_button)
-        layout.addWidget(self.exit_button)
-
-        # Set layout for central widget
-        central_widget.setLayout(layout)
-
-        # Set central widget
-        self.setCentralWidget(central_widget)
-
-    def set_red_led(self):
-        GPIO.output(RED_LED_PIN, True)
-        GPIO.output(GREEN_LED_PIN, False)
-        GPIO.output(BLUE_LED_PIN, False)
-
-    def set_green_led(self):
-        GPIO.output(RED_LED_PIN, False)
-        GPIO.output(GREEN_LED_PIN, True)
-        GPIO.output(BLUE_LED_PIN, False)
-
-    def set_blue_led(self):
-        GPIO.output(RED_LED_PIN, False)
-        GPIO.output(GREEN_LED_PIN, False)
-        GPIO.output(BLUE_LED_PIN, True)
-
-if _name_ == "_main_":
-    app = QApplication(sys.argv)
-    window = LEDController()
-    window.show()
-    sys.exit(app.exec_())
-
-# Cleanup GPIO on exit
-GPIO.cleanup()
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.LOW) GPIO.setup(22,GPIO.LOW) GPIO.setup(17,GPIO.LOW)
+win =tk.Tk() win.title("LED Blink YAYY") win.geometry('800x600')
+def green(): if GPIO.input(27): GPIO.output(27, GPIO.LOW) green_button.config(text="Green Led: OFF") else: GPIO.output(27, GPIO.HIGH) GPIO.output(22, GPIO.LOW) GPIO.output(17, GPIO.LOW) green_button.config(text="Green Led: ON")
+def blue(): if GPIO.input(22): GPIO.output(22, GPIO.LOW) blue_button.config(text="Blue Led: OFF") else: GPIO.output(22, GPIO.HIGH) GPIO.output(27, GPIO.LOW) GPIO.output(17, GPIO.LOW) blue_button.config(text="Blue Led: ON")
+def red(): if GPIO.input(17): GPIO.output(17, GPIO.LOW) red_button.config(text="Red Led: OFF") else: GPIO.output(17, GPIO.HIGH) GPIO.output(27, GPIO.LOW) GPIO.output(22, GPIO.LOW) red_button.config(text="Red Led: ON")
+green_button = tk.Radiobutton(win, text="Green Led: OFF", command=green) green_button.pack(pady=10) blue_button = tk.Radiobutton(win, text="Blue Led: OFF", command=blue) blue_button.pack(pady=10) red_button = tk.Radiobutton(win, text="Red Led: OFF", command=red) red_button.pack(pady=10)
+def exit(): GPIO.cleanup() win.quit()
+exit_button = tk.Button(win, text="Exit", command=exit) exit_button.pack(pady=10)
+win.mainloop()
